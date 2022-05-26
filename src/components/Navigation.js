986b6d73dbba2938b3eac611e84c3dd22b81f427
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-05-26 21:54:58 +0100

    complete header UI design

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import Scoreboard from "./Scoreboard";
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <h1>ANIMAL MEMORY GAME</h1>
+      <Scoreboard />
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,16 @@
+function Scoreboard() {
+  return (
+    <ul>
+      <li>
+        Score
+        <p>0</p>
+      </li>
+      <li>
+        Best Score
+        <p>0</p>
+      </li>
+    </ul>
+  );
+}
+
+export default Scoreboard;
