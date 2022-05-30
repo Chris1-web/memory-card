b19e3bad305d23974f1e63095f54123ccf615ef6
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-05-30 16:36:34 +0100

    change navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,8 @@ import Scoreboard from "./Scoreboard";
 function Navigation(props) {
   return (
     <div className="navigation">
-      <h1>ANIMAL MEMORY GAME</h1>
       <Scoreboard score={props.score} bestScore={props.bestScore} />
+      <h2>ANIMAL MEMORY GAME</h2>
     </div>
   );
 }
